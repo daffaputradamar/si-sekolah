be26134429f522af122987abc95c50ff0afa8437
Daffa Akbar <daffaputradamar@gmail.com>
2020-10-28 20:44:05 +0700

    change kepsek

--- application/views/depan/v_kepsek.php
+++ application/views/depan/v_kepsek.php
@@ -4,19 +4,17 @@
             <div class="col-lg-12">
                 <div class="single_event d-flex align-items-center h-100">
                     <div class="thumb">
-                        <img src="<?php echo base_url(''); ?>style/img/kepsek.jpg" alt="kepala sekolah">
+                        <img src="<?php echo base_url(''); ?>style/img/kepsek.jpg" alt="kepala sekolah" class="img-fluid" style="min-width: 300px">
                     </div>
                     <div class="event_details_info h-100">
                         <div class="d-flex flex-column h-100">
                             <div class="event_info mb-3">
                                 <h4>SAMBUTAN KEPALA SEKOLAH</h4>
+                                <h3>Tantri Ambarsari, S.Pd., M. Eng</h3>
                             </div>
 
-                            <p>Sebagai lembaga pendidikan, SMA Negeri 1 Bayat tanggap dengan perkembangan teknologi tersebut.
-                                Dengan dukungan SDM yang di miliki sekolah ini siap untuk berkompetisi dengan sekolah lain dalam pelayanan informasi publik.
-                                Teknologi Informasi Web khususnya, menjadi sarana bagi SMA Negeri 1 Bayat untuk memberi pelayanan informasi
-                                secara cepat, jelas, dan akuntable. Dari layanan ini pula, sekolah siap menerima saran dari semua pihak yang akhirnya
-                                dapat menjawab Kebutuhan masyarakat.
+                            <p>
+                                Sebagai lembaga pendidikan, SMA Negeri 1 Bayat tanggap dengan perkembangan teknologi tersebut. Dengan dukungan SDM yang di miliki sekolah ini siap untuk berkompetisi dengan sekolah lain dalam pelayanan informasi publik. Teknologi Informasi Web khususnya, menjadi sarana bagi SMA Negeri 1 Bayat untuk memberi pelayanan informasi secara cepat, jelas, dan akuntable. Dari layanan ini pula, sekolah siap menerima saran dari semua pihak yang akhirnya dapat menjawab Kebutuhan masyarakat.
                             </p>
                         </div>
                     </div>
